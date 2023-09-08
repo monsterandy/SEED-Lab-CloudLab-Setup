ee59d20c82f49d80896d42e87849321bbaf0f432
Zheyuan (Andy) Ma <zheyuanm@buffalo.edu>
2023-09-08 16:03:56 -0400

    add docker

--- install.sh
+++ install.sh
@@ -88,21 +88,21 @@ sudo cp -r /tmp/gdbpeda /opt
 rm -rf /tmp/gdbpeda
 
 #================================================
-# echo "Installing docker utilities ..."
+echo "Installing docker utilities ..."
   
-# # Install docker
-# sudo apt -y install docker.io
+# Install docker
+sudo apt -y install docker.io
 
-# # Start docker and enable it to start after the system reboot:
-# sudo systemctl enable --now docker
+# Start docker and enable it to start after the system reboot:
+sudo systemctl enable --now docker
 
-# # Optionally give any user administrative privileges to docker:
-# sudo usermod -aG docker $USERID
+# Optionally give any user administrative privileges to docker:
+sudo usermod -aG docker $USERID
 
-# # Install docker-compose. Check whether 1.27.4 is the newest version
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Install docker-compose. Check whether 1.27.4 is the newest version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-# sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 
 #================================================
