104cb03f406f54ad91ac3e735d9aef23466f9c35
Zheyuan (Andy) Ma <zheyuanm@buffalo.edu>
2023-10-18 15:33:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -101,7 +101,7 @@ sudo systemctl enable --now docker
 sudo usermod -aG docker $USERID
 
 # Install docker-compose. Check whether 1.27.4 is the newest version
-sudo curl -L "https://github.com/docker/compose/releases/download/2.20.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
